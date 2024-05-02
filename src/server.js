4d503536d4faf06e5b63ecb6b9fb4bb73ae76e26
Allan Juma <allancjuma@gmail.com>
2024-05-02 09:01:12 +0300

    add: database config, server initialization

---
+++ src/server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const sequelize = require("./config/database");
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+function startServer() {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connection has been established successfully.");
+    return sequelize.sync({ alter: true });
+  })
+  .then(() => {
+    console.log("Database synced");
+    startServer();
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
