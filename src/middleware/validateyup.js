6a90e21a5384d400888801fee0ae3554ad9a63fd
Allan Juma <allancjuma@gmail.com>
2024-05-05 21:14:22 +0300

    update: validate yup middleware  to handle req.query

--- src/middleware/validateyup.js
+++ src/middleware/validateyup.js
@@ -1,13 +1,15 @@
-const formValidate = (schema) => async (req, res, next) => {
-  const body = req.body;
-  try {
-    await schema.validate(body);
-    next();
-  } catch (error) {
-    const YupValidationError = new Error(error.message);
-    YupValidationError.name = "YupValidationError";
-    next(YupValidationError);
-  }
-};
+const formValidate =
+  (schema, useQuery = false) =>
+  async (req, res, next) => {
+    const data = useQuery ? req.query : req.body;
+    try {
+      await schema.validate(data);
+      next();
+    } catch (error) {
+      const YupValidationError = new Error(error.message);
+      YupValidationError.name = "YupValidationError";
+      next(YupValidationError);
+    }
+  };
 
 module.exports = formValidate;
