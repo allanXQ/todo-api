b8500da0b3aae4694ae124bde8b855a395b6b1de
Allan Juma <allancjuma@gmail.com>
2024-05-05 23:09:59 +0300

    fix: sequelize importations

--- src/models/todo.js
+++ src/models/todo.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
-const sequelize = require("@config");
+const { sequelize } = require("@config");
 const User = require("./user");
 
 class Todo extends Model {}
