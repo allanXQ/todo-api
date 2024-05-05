dc3359fd8031538e1e140cb6224380da2481087f
Allan Juma <allancjuma@gmail.com>
2024-05-05 21:07:06 +0300

    add; todo yup validation schemas

--- src/yupschemas/index.js
+++ src/yupschemas/index.js
@@ -28,9 +28,46 @@ const resetPasswordSchema = yup.object().shape({
     .required(),
 });
 
+const titleSchema = yup.string().trim().required(messages.todoTitleRequired);
+const descriptionSchema = yup.string().trim().nullable();
+
+const addTodoSchema = yup.object().shape({
+  title: titleSchema,
+  description: descriptionSchema,
+  userId: yup.number().integer().positive().required(messages.userIdRequired), // Assuming userId is required
+});
+
+const updateTodoSchema = yup.object().shape({
+  id: yup.number().integer().positive().required(messages.todoIdRequired),
+  title: titleSchema.notRequired(),
+  description: descriptionSchema.notRequired(),
+});
+
+const getTodosSchema = yup.object().shape({
+  filter: yup.string().trim().nullable(),
+  sort: yup
+    .string()
+    .oneOf(["createdAt", "title"], messages.invalidSortField)
+    .default("createdAt"),
+  order: yup
+    .string()
+    .oneOf(["ASC", "DESC"], messages.invalidSortOrder)
+    .default("ASC"),
+  page: yup.number().integer().positive().default(1),
+  pageSize: yup.number().integer().positive().default(10),
+});
+
+const deleteTodoSchema = yup.object().shape({
+  id: yup.number().integer().positive().required(messages.todoIdRequired),
+});
+
 module.exports = {
   regSchema,
   loginSchema,
   forgotPasswordSchema,
   resetPasswordSchema,
+  addTodoSchema,
+  updateTodoSchema,
+  getTodosSchema,
+  deleteTodoSchema,
 };
