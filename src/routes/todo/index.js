bd4432993d40efdd1966a4384bfca0a1920dc7da
Allan Juma <allancjuma@gmail.com>
2024-05-05 21:35:24 +0300

    add: yup validation middleware for todo routes

--- src/routes/todo/index.js
+++ src/routes/todo/index.js
@@ -6,21 +6,20 @@ import {
   updateTodo,
   deleteTodo,
 } from "../../controllers/todos";
-// import {
-//   regSchema,
-//   loginSchema,
-//   forgotPasswordSchema,
-//   resetPasswordSchema,
-// } from "../../yupschemas";
+import {
+  addTodoSchema,
+  updateTodoSchema,
+  getTodosSchema,
+  deleteTodoSchema,
+} from "../../yupschemas";
 
 import { errorHOC } from "../../utils";
 
 const router = Router();
 
-// routes
-router.post("/add", errorHOC(addTodo));
-router.get("/", errorHOC(getTodos));
-router.post("/update", errorHOC(updateTodo));
-router.post("/delete", errorHOC(deleteTodo));
+router.post("/add", formValidate(addTodoSchema), errorHOC(addTodo));
+router.get("/", formValidate(getTodosSchema), errorHOC(getTodos));
+router.post("/update", formValidate(updateTodoSchema), errorHOC(updateTodo));
+router.post("/delete", formValidate(deleteTodoSchema), errorHOC(deleteTodo));
 
 module.exports = router;
